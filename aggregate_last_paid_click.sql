2ea39d6164368f6e69d9c940e321cfea98e7cf70
Svetlana Movchan <movchan.svetlana07@gmail.com>
2024-10-29 12:26:01 +0300

    Added aggregate_last_paid_click files csv & sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -10,7 +10,10 @@ WITH tab AS (
         l.amount,
         l.closing_reason,
         l.status_id,
-        to_char(s.visit_date, 'dd-mm-yyyy') AS visit_date
+        to_char(s.visit_date, 'yyyy-mm-dd') AS visit_date,
+        row_number()
+            OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC)
+        AS row_n
     FROM sessions AS s
     LEFT JOIN
         leads AS l
@@ -20,14 +23,14 @@ WITH tab AS (
 
 total AS (
     SELECT
-        to_char(campaign_date, 'dd-mm-yyyy') AS campaign_date,
+        to_char(campaign_date, 'yyyy-mm-dd') AS campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
         sum(daily_spent) AS total_cost
     FROM vk_ads
     GROUP BY
-        to_char(campaign_date, 'dd-mm-yyyy'),
+        to_char(campaign_date, 'yyyy-mm-dd'),
         utm_source,
         utm_medium,
         utm_campaign
@@ -35,14 +38,14 @@ total AS (
     UNION ALL
 
     SELECT
-        to_char(campaign_date, 'dd-mm-yyyy') AS campaign_date,
+        to_char(campaign_date, 'yyyy-mm-dd') AS campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
         sum(daily_spent) AS total_cost
     FROM ya_ads
     GROUP BY
-        to_char(campaign_date, 'dd-mm-yyyy'),
+        to_char(campaign_date, 'yyyy-mm-dd'),
         utm_source,
         utm_medium,
         utm_campaign
@@ -57,7 +60,7 @@ SELECT
     count(visitor_id) AS visitors_count,
     count(lead_id) AS leads_count,
     count(lead_id) FILTER (WHERE status_id = 142) AS purchases_count,
-    sum(amount) AS revenue
+    sum(amount) FILTER (WHERE status_id = 142) AS revenue
 FROM tab AS t
 LEFT JOIN
     total AS tl
@@ -66,6 +69,7 @@ LEFT JOIN
         AND t.source = tl.utm_source
         AND t.medium = tl.utm_medium
         AND t.campaign = tl.utm_campaign
+WHERE row_n =1
 GROUP BY visit_date, source, medium, campaign, total_cost
 ORDER BY
     revenue DESC NULLS LAST,
